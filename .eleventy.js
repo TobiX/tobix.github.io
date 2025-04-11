cb5da5138451af6fcfb98190e98fad3150600906
Tobias Gruetzmacher <tobias-git@23.gs>
2025-04-11 18:00:58 +0200

    Add Marvel Heroes patch that really fixes the problem

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 // Config for Eleventy
 export default async function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("mh-linux-patch/*.xdelta");
+	eleventyConfig.addPassthroughCopy("mh-linux-patch/*.zip");
 	eleventyConfig.addPassthroughCopy("mh-linux-patch/rom-patcher-js");
 };
