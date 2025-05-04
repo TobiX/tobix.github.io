39eecd982c8369ffacd2810db46256500a99fde4
Tobias Gruetzmacher <tobias-git@23.gs>
2025-05-04 18:19:04 +0200

    Add a note about browser download warnings

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 // Config for Eleventy
 export default async function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("mh-linux-patch/*.png");
 	eleventyConfig.addPassthroughCopy("mh-linux-patch/*.zip");
 	eleventyConfig.addPassthroughCopy("mh-linux-patch/rom-patcher-js");
 };
